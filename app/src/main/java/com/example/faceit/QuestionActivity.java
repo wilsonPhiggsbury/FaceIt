a2be725ed0a8164a21069556486643393fb4795a
misterdeacon <misterdeacon@users.noreply.github.com>
2018-09-27 23:18:54 +0800

    Question activity layout

---
+++ app/src/main/java/com/example/faceit/QuestionActivity.java
@@ -0,0 +1,13 @@
+package com.example.faceit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuestionActivity extends AppCompatActivity {
+    private TextView
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question);
+    }
+}
