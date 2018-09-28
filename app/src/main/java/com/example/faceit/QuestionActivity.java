94fc65d0929374c827a8fe1e1e9d1507a490fbd8
misterdeacon <misterdeacon@users.noreply.github.com>
2018-09-28 13:15:09 +0800

    Add in textview for instruction page

--- app/src/main/java/com/example/faceit/QuestionActivity.java
+++ app/src/main/java/com/example/faceit/QuestionActivity.java
@@ -2,9 +2,32 @@ package com.example.faceit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class QuestionActivity extends AppCompatActivity {
-    private TextView
+    private TextView countLabel;
+    private TextView questionLabel;
+    private Button answerBtn1;
+    private Button answerBtn2;
+    private Button answerBtn3;
+    private Button answerBtn4;
+    private String rightAnswer;
+    private int rightAnswerCount = 0;
+    private int quizCount = 1;
+
+//    ArrayList<ArrayList<String>> quizArray = new ArrayList<>();
+//
+//    String quizData[][] = {
+//            // {"Question","Right Answer", "Choice 1", "Choice 2", "Choice 3", "Choice 4"}
+//            {""}
+//    }
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
