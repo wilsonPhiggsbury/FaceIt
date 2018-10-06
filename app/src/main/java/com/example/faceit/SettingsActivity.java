5e6684ab46bb6116b6f1efbc02aecf8f0603a0fd
misterdeacon <misterdeacon@users.noreply.github.com>
2018-10-06 15:37:41 +0800

    add in toast message upon succesful upload in SettingsActivity

--- app/src/main/java/com/example/faceit/SettingsActivity.java
+++ app/src/main/java/com/example/faceit/SettingsActivity.java
@@ -1,5 +1,6 @@
 package com.example.faceit;
 
+import android.content.Context;
 import android.content.ContextWrapper;
 import android.content.Intent;
 import android.database.Cursor;
@@ -9,11 +10,13 @@ import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.AlteredCharSequence;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.security.Timestamp;
 
@@ -48,6 +51,8 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
 
             case R.id.uploadImageBtn:
                 uploadImageToInternalStorage(imageToUpload);
+                Toast toast = Toast.makeText(getApplicationContext(), "Image Uploaded!", Toast.LENGTH_SHORT);
+                toast.show();
                 break;
 
         }
